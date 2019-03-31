ae12b852a310deaf2de6efcbd136edd014e36b87
Dan Williams <daniel@chat.za.net>
2019-03-31 20:00:28 +0100

    added some unit tests

---
+++ src/test/java/com/eetchyza/springauth/AuthServiceTest.java
@@ -0,0 +1,99 @@
+package com.eetchyza.springauth;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+
+import com.eetchyza.springauth.api.GrantedAuthority;
+import com.eetchyza.springauth.api.UserDetails;
+import com.eetchyza.springauth.api.UserDetailsService;
+import com.eetchyza.springauth.exceptions.UsernameOrPasswordIncorrectException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AuthServiceTest {
+	private UserDetailsService userDetailsService;
+
+	private AuthService authService;
+
+	@Before
+	public void setup() {
+		userDetailsService = mock(UserDetailsService.class);
+		authService = new AuthService(userDetailsService);
+	}
+
+	@Test(expected = UsernameOrPasswordIncorrectException.class)
+	public void testLogin_noUser() throws UsernameOrPasswordIncorrectException {
+		String username = "bad-bob";
+		UserDetails user = createUser(4L, "STANDARD", "test-pass", LocalDateTime.now().plusHours(3));
+		when(userDetailsService.loadUserByUsername(username)).thenReturn(null);
+
+		authService.login(username, user.getPassword());
+	}
+
+	@Test(expected = UsernameOrPasswordIncorrectException.class)
+	public void testLogin_incorrectPassword() throws UsernameOrPasswordIncorrectException {
+		UserDetails user = createUser(6L, "STANDARD", authService.hashAndSalt("test-pass"), LocalDateTime.now().plusHours(3));
+		when(userDetailsService.loadUserByUsername(user.getUsername())).thenReturn(user);
+
+		authService.login(user.getUsername(), "bad-password");
+	}
+
+	@Test
+	public void testLogin_success() throws UsernameOrPasswordIncorrectException {
+		UserDetails user = createUser(6L, "STANDARD", authService.hashAndSalt("test-pass"), LocalDateTime.now().plusHours(3));
+		when(userDetailsService.loadUserByUsername(user.getUsername())).thenReturn(user);
+
+		Authentication actual = authService.login(user.getUsername(), "test-pass");
+		assertThat(actual.isExpired()).isFalse();
+	}
+
+	@Test
+	public void testPasswordsMatch_true(){
+		String password = "test-password";
+		assertThat(authService.passwordsMatch(password, authService.hashAndSalt(password))).isTrue();
+	}
+
+	@Test
+	public void testPasswordsMatch_false(){
+		assertThat(authService.passwordsMatch("test-password", authService.hashAndSalt("bad-password"))).isFalse();
+	}
+
+	private UserDetails createUser(long id, String auth, String password, LocalDateTime expires) {
+		return new UserDetails() {
+			@Override
+			public long getId() {
+				return id;
+			}
+
+			@Override
+			public Collection<? extends GrantedAuthority> getAuthorities() {
+				return Collections.singletonList((GrantedAuthority) () -> auth);
+			}
+
+			@Override
+			public String getPassword() {
+				return password;
+			}
+
+			@Override
+			public String getUsername() {
+				return "test-user";
+			}
+
+			@Override
+			public boolean isTemporaryPassword() {
+				return false;
+			}
+
+			@Override
+			public LocalDateTime getExpires() {
+				return expires;
+			}
+		};
+	}
+}
---
+++ src/test/java/com/eetchyza/springauth/AuthenticationTest.java
@@ -0,0 +1,65 @@
+package com.eetchyza.springauth;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+
+import com.eetchyza.springauth.api.GrantedAuthority;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class AuthenticationTest {
+
+	private String authenticationToken;
+	private String refreshToken;
+	private LocalDateTime expire;
+	private Collection<? extends GrantedAuthority> roles;
+	private String username;
+	private long id;
+	private Authentication authentication;
+
+	@Before
+	public void setup(){
+		authenticationToken = "test-auth-token";
+		refreshToken = "test-refresh-token";
+		expire = LocalDateTime.now().plusHours(1);
+		roles = Collections.singletonList((GrantedAuthority) () -> "STANDARD");
+		username = "test-user";
+		id = 5L;
+		authentication = new Authentication(authenticationToken, refreshToken, expire, roles, username, id);
+	}
+
+	@Test
+	public void testIsRefreshToken_true(){
+		assertTrue(authentication.isRefreshToken(refreshToken));
+	}
+
+	@Test
+	public void testIsRefreshToken_false(){
+		assertFalse(authentication.isRefreshToken("234097ghhjh"));
+	}
+
+	@Test
+	public void testIsExpired_true(){
+		authentication = new Authentication(authenticationToken, refreshToken, LocalDateTime.now().minusHours(2), roles, username, id);
+		assertTrue(authentication.isExpired());
+	}
+
+	@Test
+	public void testIsExpired_false(){
+		assertFalse(authentication.isExpired());
+	}
+
+	@Test
+	public void testHasRoles_true(){
+		assertTrue(authentication.hasRoles(new String[]{"STANDARD"}));
+	}
+
+	@Test
+	public void testHasRoles_false(){
+		assertFalse(authentication.hasRoles(new String[]{"ADMIN"}));
+	}
+}
